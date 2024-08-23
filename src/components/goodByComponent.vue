6f80d1ef487bf8f44704f9cf238014570400060b
Kristaps <steinskristaps94@gmail.com>
2024-08-23 23:42:12 +0300

    Update goodbye message in goodByComponent.vue

--- src/components/goodByComponent.vue
+++ src/components/goodByComponent.vue
@@ -1,17 +1,12 @@
 <template>
-    <div>
-        <h1>Paldies</h1>
-    </div>
+  <div>
+    <h1>Paldies!!!</h1>
+    <h3>Atceramies ka arī 15. septembri vajag brīvu :)</h3>
+  </div>
 </template>
 
 <script>
 export default {
   name: "GoodByComponent",
-  data() {
-  },
-  computed: {
-  },
-  methods: {
-  },
 };
 </script>
